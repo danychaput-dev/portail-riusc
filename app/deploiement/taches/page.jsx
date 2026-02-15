e4029edd2275c03fd8d097ead265a8dd32bab13b
Dany Chaput <dany.chaput@aqbrs.ca>
2026-02-15 16:55:54 -0500

    Ajout fiches de tâches RIUSC

--- app/deploiement/taches/page.jsx
+++ app/deploiement/taches/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import FichesTachesRIUSC from "@/components/FichesTaches";
+import FichesTachesRIUSC from "../components/FichesTaches";
 
 export default function PageTaches() {
   return <FichesTachesRIUSC />;
